fdf752320d160fdd99114f94c3251f15625148e7
Zach Wilson <zwilson22495@gmail.com>
2022-01-21 16:14:57 -0800

    Added and finished Controller. Seeded Animals

--- Shelter/Models/ShelterContext.cs
+++ Shelter/Models/ShelterContext.cs
@@ -10,5 +10,17 @@ namespace Shelter.Models
         }
 
         public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Animal>()
+          .HasData(
+            new Animal { AnimalId = 1, Name = "Rufus", Species = "French Bulldog", Age = 3, Gender = "Male" },
+            new Animal { AnimalId = 2, Name = "Felix", Species = "Siamese Cat", Age = 6, Gender = "Male" },
+            new Animal { AnimalId = 3, Name = "Annabelle", Species = "English Springer Spaniel", Age = 5, Gender = "Female" },
+            new Animal { AnimalId = 4, Name = "Tater Tot", Species = "Pomeranian Dog", Age = 2, Gender = "Male" },
+            new Animal { AnimalId = 5, Name = "Princess", Species = "Persian Cat", Age = 1, Gender = "Male" }
+      );
+}
     }
 }
