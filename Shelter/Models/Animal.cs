17abe28192401debdc2e046113acffcf3a19e030
Zach Wilson <zwilson22495@gmail.com>
2022-01-24 02:17:06 -0800

    Pushed to GitHub

--- Shelter/Models/Animal.cs
+++ Shelter/Models/Animal.cs
@@ -3,10 +3,16 @@ namespace Shelter.Models
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(30)]
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
+    [Required]
     public string Breed { get; set; }
+    [Required]
     public int Age { get; set; }
+    [Required]
     public string Sex { get; set; }
   }
 }
